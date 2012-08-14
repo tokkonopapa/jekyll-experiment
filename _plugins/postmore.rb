666b4495a5dbd0571ade53dde54ede0fdf9f4c57
tokkonopapa <tokkonopapa@yahoo.com>
2012-08-14 22:11:53 +0900

    update postmore.rb

--- _plugins/postmore.rb
+++ _plugins/postmore.rb
@@ -1,11 +1,13 @@
-module PostMore
-  def postmorefilter(input)
-    if input.include? "<!--more-->"
-      input.split("<!--more-->").first
-    else
-      input
+module Jekyll
+  module PostMore
+    def postmorefilter(input)
+      if input.include? "<!--more-->"
+        input.split("<!--more-->").first
+      else
+        input
+      end
     end
   end
 end
 
-Liquid::Template.register_filter(PostMore)
+Liquid::Template.register_filter(Jekyll::PostMore)
