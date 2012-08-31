bd90765e061bd389004d88d07e87e73f0569ce44
tokkonopapa <tokkonopapa@yahoo.com>
2012-08-31 22:51:33 +0900

    add script template

---
+++ _includes/script/twitter.js
@@ -0,0 +1,103 @@
+{% if site.asides.tweets and site.twitter %}
+/*
+ * Twitter widget
+ */
+;(function($, window, document) {
+	$.fn.showTweets = function(username, options) {
+		options = $.extend({}, $.fn.showTweets.options, options);
+
+		var getTweets = function(user, opts) {
+			return $.ajax({
+				url: 'http://api.twitter.com/1/statuses/user_timeline/' + user + '.json',
+				data: opts,
+				dataType: 'jsonp'
+			});
+		};
+
+		var putTweets = function(elm, args) {
+			elm.empty().hide();
+			var n = args.length-1;
+			var now = new Date();
+			$.each(args, function(i, tweet) {
+				if (tweet.text !== undefined) {
+					elm.append('<li><p>' + makeLinks(tweet.text) + '&nbsp;' + 
+					'<a href="http://twitter.com/' + username + '/status/' + tweet.id_str + '">' + 
+					'<span class="badge">' + prettyDate(now, tweet.created_at) + '</span></a></p></li>');
+					if (i < n) {
+						elm.append('<li class="divider"></li>');
+					}
+				}
+			});
+			elm.fadeIn();
+		};
+
+		var makeLinks = function(text) {
+			return text.replace(/(https?:\/\/)([\w\-:;?&=+.%#\/]+)/gi, '<a href="$1$2">$2</a>')
+				.replace(/(^|\W)@(\w+)/g, '$1<a href="http://twitter.com/$2">@$2</a>')
+				.replace(/(^|\W)#(\w+)/g, '$1<a href="http://search.twitter.com/search?q=%23$2">#$2</a>');
+		};
+
+		var say = {
+			just_now: "now",
+			minute_ago: "1m",
+			minutes_ago: "m",
+			hour_ago: "1h",
+			hours_ago: "h",
+			yesterday: "1d",
+			days_ago: "d",
+			last_week: "1w",
+			weeks_ago: "w"
+		};
+
+		var prettyDate = function(now, time) {
+			if (typeof now.getTime !== "function") {
+				return "&infin;";
+			}
+
+			var diff = (now.getTime() - (new Date(time || "")).getTime()) / 1000,
+				day_diff = Math.round(diff / 86400);
+
+			if (isNaN(day_diff) || day_diff < 0 || day_diff >= 31) {
+				return "&infin;";
+			}
+
+			return day_diff === 0 && (
+			  diff <    60 && say.just_now || 
+			  diff <   120 && say.minute_ago || 
+			  diff <  3600 && Math.floor(diff / 60) + say.minutes_ago || 
+			  diff <  7200 && say.hour_ago || 
+			  diff < 86400 && Math.floor(diff / 3600) + say.hours_ago) || 
+			day_diff === 1 && say.yesterday || 
+			day_diff <   7 && day_diff + say.days_ago || 
+			day_diff === 7 && say.last_week || 
+			day_diff >   7 && Math.ceil(day_diff / 7) + say.weeks_ago;
+		};
+
+		var n = options.count;
+		options.count += 5;
+
+		return this.each(function() {
+			var elem = $(this);
+
+			getTweets(username, options).done(function(args) {
+				putTweets(elem, args.slice(0, n));
+			});
+		});
+	};
+
+	$.fn.showTweets.options = {
+		count: 5,
+		trim_user: true,
+		include_entities: true,
+		exclude_replies: true
+	};
+
+})(jQuery, window, document);
+
+$(function () {
+	$('#tweets').showTweets('{{ site.twitter.username }}', {
+		count: {{ site.twitter.count }},
+		exclude_replies: {{ site.twitter.show_replies }}
+	});
+});
+{% endif %}
