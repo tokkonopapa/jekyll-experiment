84771e4200b0fe3f5dd044faff5983420e335b56
tokkonopapa <tokkonopapa@yahoo.com>
2012-09-01 21:02:04 +0900

    update project page

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,7 @@
  
 {% if site.asides.tweets and site.twitter %}{% include js/twitter.js %}{% endif %}
 {% if site.asides.github and site.github  %}{% include js/github.js  %}{% endif %}
+
+$(function () {
+	$('.carousel').carousel();
+});
