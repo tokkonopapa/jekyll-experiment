ee887303c4923c46682cd0ab895447e59157b225
tokkonopapa <tokkonopapa@yahoo.com>
2012-09-15 00:44:33 +0900

    update bootstrap structores

---
+++ assets/bootstrap-2.1.1/js/bootstrap.custom.js
@@ -0,0 +1,7 @@
+---
+---
+{% include bootstrap-2.1.1/js/bootstrap-carousel.js %}
+{% include bootstrap-2.1.1/js/bootstrap-collapse.js %}
+{% include bootstrap-2.1.1/js/bootstrap-dropdown.js %}
+{% include bootstrap-2.1.1/js/bootstrap-tab.js %}
+{% include bootstrap-2.1.1/js/bootstrap-transition.js %}
