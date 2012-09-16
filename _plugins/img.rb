0881408ee015e6249927e5476aa214316b086c0a
tokkonopapa <tokkonopapa@yahoo.com>
2012-09-16 23:31:09 +0900

    update img.rb

--- _plugins/img.rb
+++ _plugins/img.rb
@@ -1,19 +1,21 @@
 # This is a sample of jekyll plugin just for explaining how to parse & render.
 #
 # Usage:
-#    {% img /assets/img/sample.jpg 'some text' %}
+#    {% img /assets/img/sample.jpg I&#39;ll be back %}
 # Result:
-#    <img src='/assets/img/sample.jpg' class='thumbnail' alt='some text' title='some text'>
+#    <img src='/assets/img/sample.jpg' alt='I&#39;ll be back' title='I&#39;ll be back'>
 
 module Jekyll
   class Img < Liquid::Tag
     def initialize(tag_name, args, tokens)
       super
-      @args = args.split(" ", 2)
+      p = args.split(" ", 2)
+      @src = p[0].gsub(/['"]/, '').rstrip
+      @alt = p[1].gsub(/['"]/, '').rstrip
     end
 
     def render(context)
-      "<img src='" + @args[0] + "' class='thumbnail' style='margin-left:auto; margin-right:auto' alt='" + @args[1] + "' title='" + @args[1] + "'>"
+      "<img src='" + @src + "' class='thumbnail' style='margin:1em auto' alt='" + @alt + "' title='" + @alt + "'>"
     end
   end
 end
